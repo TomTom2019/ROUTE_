d083772461e38a1bb80d70a1815fb5d929ccde2f
TomTom2019 <daless2000be@yandex.com>
2023-01-11 14:06:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+import Home from './components/home'
+import Posts from './components/posts'
+import Profile from './components/profile';
+
+const App = () => {
+  return(
+    <BrowserRouter>
+    <header>
+      <Link to= "">Home</Link><br/>
+      <Link to= 'posts'>Posts</Link><br/>
+      <Link to='profile'>Profile</Link><br/>
+    </header>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='posts' element={<Posts/>}/>
+      <Route path='profile' element={<Profile/>}/>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
